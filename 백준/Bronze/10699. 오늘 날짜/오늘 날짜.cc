a456cf22b9669c11e3184d6c1d1eb670f98cd4ee
이세진 <tp028@naver.com>
2025-08-05 03:47:08 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
     time_t timer = time(NULL) + 9 * 60 * 60; // 현재 시각 반환
-    struct tm* t = localtime(&timer); // tm 구조체 사용
+    struct tm* t = gmtime(&timer); // tm 구조체 사용
     int year = t->tm_year + 1900;
     int month = t->tm_mon + 1;
     int day = t->tm_mday;
