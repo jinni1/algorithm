fe3e91c6e52e1739fd77bf3f252f6bfc812b1fdc
이세진 <tp028@naver.com>
2026-03-11 13:01:51 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -4,29 +4,12 @@ using namespace std;
 int main() {
     int x;
     cin >> x;
-    int sum = 64;
-    int min = 0;
-    while ( sum != x ) {
-        if ( min == 0 ) {
-            sum = sum >> 1; //32
-            min = sum; // 32
-            if ( sum > x ){
-                min = 0;
-            }
-        } else {
-            min = min >> 1; 
-            if ( sum + min <= x ) {
-                sum = sum + min; 
-            }
-        } 
-    }
-
     int count = 0;
-    while( sum ) {
-        if ( sum % 2 == 1 ){
+    while( x ) {
+        if ( x % 2 == 1 ){
             count++;
         }
-        sum = sum/2;
+        x = x/2;
     }
     cout << count << "\n";
 }
